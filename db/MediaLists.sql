2c34d5bf49d67513f17a068192425915b4bce21a
Anton Ralph Valenzuela <anton.valenzuela@obf.ateneo.edu>
2021-03-19 15:37:55 +0800

    Switched deletion rules to cascade.

--- db/MediaLists.sql
+++ db/MediaLists.sql
@@ -22,7 +22,7 @@ CREATE TABLE episode(
     season_no INT NOT NULL,
     episode_no INT NOT NULL,
     media_id INT NOT NULL,
-    FOREIGN KEY(media_id) REFERENCES media(media_id) ON DELETE RESTRICT
+    FOREIGN KEY(media_id) REFERENCES media(media_id) ON DELETE CASCADE
 );
 
 CREATE TABLE comment(
@@ -30,14 +30,14 @@ CREATE TABLE comment(
     comment_text VARCHAR(255) NOT NULL,
     media_id INT NOT NULL,
     username VARCHAR(255) NOT NULL,
-    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE RESTRICT,
-    FOREIGN KEY (username) REFERENCES user(username) ON DELETE RESTRICT
+    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE,
+    FOREIGN KEY (username) REFERENCES user(username) ON DELETE CASCADE
 );
 
 CREATE TABLE history(
     history_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    FOREIGN KEY (username) references user(username) ON DELETE RESTRICT
+    FOREIGN KEY (username) references user(username) ON DELETE CASCADE
 );
 
 CREATE TABLE media_history(
@@ -45,23 +45,23 @@ CREATE TABLE media_history(
     media_id INT NOT NULL,
     watch_date DATE NOT NULL,
     PRIMARY KEY (history_id, media_id),
-    FOREIGN KEY (history_id) REFERENCES history(history_id) ON DELETE RESTRICT,
-    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE RESTRICT
+    FOREIGN KEY (history_id) REFERENCES history(history_id) ON DELETE CASCADE,
+    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE
 );
 
 CREATE TABLE list(
     list_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     list_name VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    FOREIGN KEY (username) REFERENCES user(username) ON DELETE RESTRICT
+    FOREIGN KEY (username) REFERENCES user(username) ON DELETE CASCADE
 );
 
 CREATE TABLE media_list(
     list_id INT NOT NULL,
     media_id INT NOT NULL,
     PRIMARY KEY (list_id, media_id),
-    FOREIGN KEY (list_id) REFERENCES list(list_id) ON DELETE RESTRICT,
-    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE RESTRICT
+    FOREIGN KEY (list_id) REFERENCES list(list_id) ON DELETE CASCADE,
+    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE
 );
 
 CREATE TABLE rating(
@@ -69,6 +69,6 @@ CREATE TABLE rating(
     score INT NOT NULL CHECK (score>0 AND score<=10),
     media_id INT NOT NULL,
     username VARCHAR(255) NOT NULL,
-    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE RESTRICT,
-    FOREIGN KEY (username) REFERENCES user(username) ON DELETE RESTRICT
+    FOREIGN KEY (media_id) REFERENCES media(media_id) ON DELETE CASCADE,
+    FOREIGN KEY (username) REFERENCES user(username) ON DELETE CASCADE
 );
